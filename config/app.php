b9abf8b1f235547b7547a63b62c93b5f1980ac94
rafaelssilva35 <rafaelssilva35@gmail.com>
2019-06-13 17:05:05 -0300

    teste

--- config/app.php
+++ config/app.php
@@ -163,6 +163,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Orphans\GitDeploy\GitDeployServiceProvider::class,
+
 
         /*
          * Package Service Providers...
